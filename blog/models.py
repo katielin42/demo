cfd03dedd0c4982647bbdc9c8c7af5bd623c7f7e
jvent <justine.ventura14@gmail.com>
2020-04-29 17:24:58 -0600

    changed links

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Blog(models.Model):
     publication = models.DateTimeField()
     body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def __str_(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
 
+from .models import Blog
 # Create your views here.
+
+
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {"blogs": blogs})
+
+
+def render_to_response(param):
+    pass
+
+
+def detail(request, blog_id):
+    '''
+    detail_blog = get_object_or_404(Blog,pk=blog_id)
+    if detail_blog== Http404:
+        print("error msg")
+        return render(request, 'blog/error.html')
+    print("here")
+    '''
+    try:
+        detail_blog = Blog.objects.get(pk=blog_id)
+        return render(request, 'blog/detail.html', {"blog": detail_blog})
+    except Blog.DoesNotExist:
+        return render(request, 'blog/error.html')
