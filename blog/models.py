ec9d3fcc4045b55bcddf9c072182b4a336630c4b
jvent <justine.ventura14@gmail.com>
2020-04-29 11:54:23 -0600

    added blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=240)
+    publication = models.DateTimeField()
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Job
 # Create your views here.
+
+def home(request):
+    # get the jobs from model, pass to be returned
+    jobs= Job.objects
+    return render(request, 'home.html', {'jobs':jobs})
