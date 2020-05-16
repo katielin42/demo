fa8e03b5830150d169c8b2908981c0cbc07b7ed1
Katie Lin <chiachen@ualberta.ca>
2020-05-16 16:58:06 -0600

    Adjust buildpacks on Heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'newsletter.apps.NewsletterConfig',
     'ourTeam.apps.TeamConfig',
 ]
