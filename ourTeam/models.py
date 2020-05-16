231edef60f9d8fe24d4b533f5fdf1bef930e339e
jvent <justine.ventura14@gmail.com>
2020-05-16 12:37:54 -0600

    friggin works wow

--- ourTeam/models.py
+++ ourTeam/models.py
@@ -5,8 +5,15 @@ class Team(models.Model):
     profile_pic = models.ImageField(upload_to='picx', default = "EELogo_Temp.png")
     name = models.CharField(max_length=240)
     position = models.CharField(max_length=21, default = "Something Rep.") # crop to fit the club.html cards
+    id_name =  models.CharField(max_length=25, default = "somethingrep")
     description_1 = models.TextField(default="Shall be responsible for ...")
+    description_2 = models.TextField(default="Shall be responsible for ...")
+    description_3 = models.TextField(default="Shall be responsible for ...")
+    description_4 = models.TextField(default="Shall be responsible for ...")
+    description_5 = models.TextField(default="Shall be responsible for ...")
     email = models.EmailField()
+    morefunc = models.TextField(default="'return more('<id>');'")
+    lessfunc = models.TextField(default="'return less('<id>');'")
 
 
     def __str__(self): # displays actual title on admin page
