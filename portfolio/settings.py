2d725601f2474af0db299b4ac9fab629103e7c68
jvent <justine.ventura14@gmail.com>
2020-05-25 14:42:41 -0600

    changed settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'eesitedb',
         'USER':'postgres',
         'PASSWORD':'newpassword',
         'HOST':'localhost',
-        'PORT':'5432', # 5433
+        'PORT':'', # 5433
     }
 }
 
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static/')
 ]
